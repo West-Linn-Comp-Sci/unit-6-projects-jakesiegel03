b9bcfde965e03d3b8e45827dc3088d124f9ec3ff
jakesiegel03 <83470602+jakesiegel03@users.noreply.github.com>
2021-05-17 12:53:11 -0700

    Update Purse.java

--- Purse.java
+++ Purse.java
@@ -1,39 +1,35 @@
-
-
-
 import java.util.ArrayList;
-public class Purse { 
-private ArrayList coins; 
- 
-    public Purse() { 
-        coins = new ArrayList(); 
+// A Purse holds a collection of Coins
+public class Purse {
+    private ArrayList<Coin> coins;
+    public Purse() {
+        coins = new ArrayList();
     }
- 
-// adds aCoin to the purse 
-public void add(Coin aCoin) { 
-coins.add(aCoin); 
-} 
- 
-// returns total value of all coins in purse 
-public double getTotal() { 
-    double total = 0;
-    for(int i = 0;i<Purse.size();i++)
-    total += Purse.get(i).getValue();
-    return total;
-} 
- 
-// returns the number of coins in the Purse that matches aCoin 
-// (both myName & myValue) 
-public int count(Coin aCoin) {
-   int num = 0;
-   for(int i = 0;i<Purse.size();i++){
-       if(aCoin.getValue()==(Purse.get(i).getValue())&&(aCoin.getName().equals(Purse.get(i).getName()))){
-           num ++;
+
+    public void add(Coin aCoin) {
+        coins.add(aCoin);
+    }
+
+    public double getTotal() {
+        double sum = 0;
+        for(Coin coin : coins) sum += coin.getValue();
+        return sum;
+    }
+
+    public int count(Coin aCoin) {
+        int count = 0;
+        for(Coin coin : coins) {
+            if(coin.equals(aCoin)) count++;
         }
+        return count;
     }
-  return num;
- } 
- 
-// returns the name of the Coin in the Purse that has the smallest value 
+
+    public String findSmallest() {
+        Coin smallestCoin = coins.get(0);
+        for(Coin coin : coins) {
+            if(coin.getValue() < smallestCoin.getValue()) smallestCoin = coin;
+        }
+        return smallestCoin.getName();
+    }
 }
 
