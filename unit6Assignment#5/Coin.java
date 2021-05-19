d55caf8c37332eae6c00281ac6328211d6461cbd
jakesiegel03 <83470602+jakesiegel03@users.noreply.github.com>
2021-05-19 10:05:17 -0700

    Update Coin.java

--- unit6Assignment#5/Coin.java
+++ unit6Assignment#5/Coin.java
@@ -1,29 +1,23 @@
-    
-public class Coin
-{
-     
-private double myValue; 
-private String myName; 
- 
-    public Coin(double value, String name) { 
-        myValue = value; 
-        myName = name; 
-    } 
- 
-    public double getValue() { 
-        return myValue; 
-    } 
- 
-    public String getName() { 
+      
+public class Coin {
+
+    private double myValue;
+    private String myName;
+
+    public Coin(double value, String name) {
+        myValue = value;
+        myName = name;
+    }
+
+    public double getValue() {
+        return myValue;
+    }
+
+    public String getName() {
         return myName;
-    }    
- 
-// returns true if this coin is equal to aCoin, 
-// which means the name and the value are both equal 
-public boolean equals(Coin aCoin, Coin Bcoin) { 
-    if(aCoin.getValue()==(Bcoin.getValue())&&(aCoin.getName().equals(Bcoin.getName())))
-    return true;
-    else
-    return false;
+    }
+
+    public boolean equals(Coin aCoin) {
+        return aCoin.getName().equals(myName) && aCoin.getValue() == myValue;
+    }
 }
-} //////////////////////////////////////////////////////////// 
